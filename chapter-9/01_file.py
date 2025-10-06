1b413fa2a67bc64d09673c70ddc993a26fd9a6fb
Sumit Lucky <you@example.com>
2025-10-06 09:33:24 +0530

    updated code

---
+++ chapter-9/01_file.py
@@ -0,0 +1,10 @@
+'''
+a = "a very long string with emails " 
+
+emails = []
+3 seconds
+'''
+f = open(r"C:\Users\sumit\OneDrive\Desktop\python programming\chapter-9\file.txt")
+data = f.read()
+print(data)
+f.close()
