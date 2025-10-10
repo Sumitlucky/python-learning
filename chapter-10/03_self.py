76114f663a0baa8cc695f01b51a1a6314e396b61
Sumit Lucky <you@example.com>
2025-10-10 23:38:35 +0530

    updated code

--- chapter-10/03_self.py
+++ chapter-10/03_self.py
@@ -12,3 +12,20 @@ sumit.language = "c++"
 sumit.getInfo()
 
 
+
+
+
+class Employee:
+  language = "pyrhon" # This is a class attribute
+  salary = 340000
+
+
+
+  def getInfo(self):
+    print(f"The language is {self.language}. The salary  {self.salary}")
+sumit  = Employee()
+
+sumit.language = "c++"
+sumit.getInfo()
+
+
