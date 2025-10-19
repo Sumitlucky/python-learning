12b32cd1f228b3e90810290d2245647215790e02
Sumit Lucky <you@example.com>
2025-10-19 11:58:50 +0530

    updated code

---
+++ chapter-12/01_walrus.py
@@ -0,0 +1,3 @@
+# Using walrus operator
+if (n := len([1,2,3,4,5])) > 3:
+  print(f"List is too long ({n} elements, expected <= 3)") #output: list is to long (5 elements, expected < = 3)
---
+++ chapter-12/03_match_case.py
@@ -0,0 +1,12 @@
+def http_status(status):
+  match status:
+    case 200:
+      return "ok"
+    case 404:
+      return "Not Found"
+    case 500:
+      return "Internal Server Error"
+    case _:
+      "Unknown status" # Usage print(http_status(200)) #Output:ok print(http_status(404)) # Not found print(http_status(500)) # Output : Internal server
+
+print(http_status(500))      
