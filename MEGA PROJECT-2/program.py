a1fa97ab4c278ef50923bb997aeeab9e483aa7c9
Sumit Lucky <you@example.com>
2025-10-22 08:13:50 +0530

    updated code

--- MEGA PROJECT-2/program.py
+++ MEGA PROJECT-2/program.py
@@ -7,8 +7,8 @@ pyautogui.click(1639,1412)
 time.sleep(1)
 
 
-pyautogui.moveTo(1003, 237)
-pyautogui.dragTo(2187,1258, duration=1.0,button='left')
+pyautogui.moveTo(547, 142)
+pyautogui.dragTo(1894,1007, duration=1.0,button='left')
 
 pyautogui.hotkey('ctrl','c')
 time.sleep(1)
